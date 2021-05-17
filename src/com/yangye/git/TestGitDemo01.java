6df7e4b76feefcf62d0f26cf847dbcac30d603f2
yangye <1596405048@qq.com>
2021-05-17 09:09:09 +0800

    将项目提交到版本库

---
+++ src/com/yangye/git/TestGitDemo01.java
@@ -0,0 +1,7 @@
+package com.yangye.git;
+
+public class TestGitDemo01 {
+    public static void main(String[] args) {
+        System.out.println("这是第一个测试类");
+    }
+}
---
+++ src/com/yangye/git/TestGitDemo02.java
@@ -0,0 +1,7 @@
+package com.yangye.git;
+
+public class TestGitDemo02 {
+    public static void main(String[] args) {
+        System.out.println("这是第二个测试类");
+    }
+}
